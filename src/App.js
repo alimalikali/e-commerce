fa9d06ed4c793e51e9a221568c60fc0970d619a7
alimalikali <alimalikali1928@gmail.com>
2023-11-14 14:55:48 +0500

    ecommerce-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AddProduct from "./components/AddProduct";
+import UpdateProduct from "./components/UpdateProduct";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path="/add" element={<AddProduct />} />
+            <Route path="/update" element={<UpdateProduct />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/AddProduct.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AddProduct() {
+  return (
+    <div>
+    <div>AddProduct</div>
+    <div>AddProduct</div>
+    <div>AddProduct</div>
+    <div>AddProduct</div>
+    <div>AddProduct</div>
+
+      
+    </div>
+  )
+}
